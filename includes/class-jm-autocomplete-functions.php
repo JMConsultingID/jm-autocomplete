396aebefe626ec2de423d7e6631ac3b5049f3b39
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-08-24 21:05:58 +0800

    Update class-jm-autocomplete-functions.php

--- includes/class-jm-autocomplete-functions.php
+++ includes/class-jm-autocomplete-functions.php
@@ -6,7 +6,9 @@ function jm_autocomplete_plugin_settings_page() {
         'AutoComplete Address',      // Judul pada menu utama
         'manage_options',            // Capability yang dibutuhkan untuk mengakses menu
         'jm_autocomplete_plugin',    // Slug menu
-        'jm_autocomplete_plugin_settings_page_content'
+        'jm_autocomplete_plugin_settings_page_content', // Callback function untuk halaman konten
+        'dashicons-location-alt',    // Ikona menu (Anda dapat mengganti dengan ikon lain)
+        75                           // Urutan di dalam menu
     );
 }
 add_action('admin_menu', 'jm_autocomplete_plugin_settings_page');
