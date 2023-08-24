c058ccfaab1af2056a129fa756b1cc6ab4295a52
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-08-24 23:16:20 +0800

    Update class-jm-autocomplete-functions.php

--- includes/class-jm-autocomplete-functions.php
+++ includes/class-jm-autocomplete-functions.php
@@ -1,16 +1,26 @@
 <?php
 // Add plugin settings page
+// Add plugin settings page
 function jm_autocomplete_plugin_settings_page() {
     add_menu_page(
-        'AutoComplete Address',      // Judul pada menu
-        'AutoComplete Address',      // Judul pada menu utama
+        'Hai Plugin',                // Judul pada menu utama
+        'Hai Plugin',                // Judul pada menu utama
         'manage_options',            // Capability yang dibutuhkan untuk mengakses menu
-        'jm_autocomplete_plugin',    // Slug menu
-        'jm_autocomplete_plugin_settings_page_content', // Callback function untuk halaman konten
+        'hai_plugin',    // Slug menu utama
         'dashicons-location-alt',    // Ikona menu (Anda dapat mengganti dengan ikon lain)
         75                           // Urutan di dalam menu
     );
+
+    add_submenu_page(
+        'hai_plugin',    // Slug menu utama ('jm_autocomplete_plugin' dari menu utama di atas)
+        'Auto Complete Address',      // Judul submenu
+        'Auto Complete Address',      // Judul submenu
+        'manage_options',            // Capability yang dibutuhkan untuk mengakses submenu
+        'jm_autocomplete_plugin', // Slug submenu
+        'jm_autocomplete_plugin_settings_page_content' // Callback function untuk halaman konten submenu
+    );
 }
+
 add_action('admin_menu', 'jm_autocomplete_plugin_settings_page');
 
 // Render settings page content
