469f3895857dac8430bae2dfbc7b533b72d66ade
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-08-30 00:54:22 +0800

    Update jm-autocomplete-main.js

--- public/js/jm-autocomplete-main.js
+++ public/js/jm-autocomplete-main.js
@@ -12,7 +12,22 @@
 
     let map;
 
-    
+    function haversineDistance(coords1, coords2) {
+        function toRad(value) {
+            return value * Math.PI / 180;
+        }
+
+        const R = 3959; // radius bumi dalam kilometer
+        const dLat = toRad(coords2[1] - coords1[1]);
+        const dLon = toRad(coords2[0] - coords1[0]);
+        const lat1 = toRad(coords1[1]);
+        const lat2 = toRad(coords2[1]);
+
+        const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return R * c;
+    }
 
     function fetchAddresses(query, resultElement) {
         const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(query)}.json?access_token=${accessToken}&type=address,place,postcode,&country=US`;
@@ -67,24 +82,7 @@
 
 
     checkCitiesAndDisplayError();
-    }
-
-    function haversineDistance(coords1, coords2) {
-        function toRad(value) {
-            return value * Math.PI / 180;
-        }
-
-        const R = 3959; // radius bumi dalam kilometer
-        const dLat = toRad(coords2[1] - coords1[1]);
-        const dLon = toRad(coords2[0] - coords1[0]);
-        const lat1 = toRad(coords1[1]);
-        const lat2 = toRad(coords2[1]);
-
-        const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        return R * c;
-    }
+    }   
 
 
     function checkCitiesAndDisplayError() {
